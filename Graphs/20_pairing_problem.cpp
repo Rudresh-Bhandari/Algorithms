5dc5324532702fbc8d4500fa810c5a02d8912d42
Rudresh <therudreshmister@gmail.com>
2022-06-05 18:36:44 +0530

    Added  graph questions that I have done over the past month

---
+++ Graphs/20_pairing_problem.cpp
@@ -0,0 +1,14 @@
+/*
+Q-There are N cities numbered from 0 to N-1. You have to choose 2 cities such that there is no path from first city to second city .
+You are given information in the form of M pairs (X,Y) i.e there is an undirected road between city X and city Y.
+Find out the number of ways in which cities can be chosen.
+
+Input Format:
+The first line contains two integers, N and M separated by a single space. M lines follow. Each line contains integers separated by a single space X and Y denoting road between X and Y.
+
+Constraints:
+1 <= N <= 100000 1 <= M <= 100000 0 <= X,Y <= N-1
+
+Output Format
+An integer that denotes the number of permissible ways to choose a pair of cities.
+*/
---
+++ Graphs/9_HOLI.cpp
@@ -0,0 +1,9 @@
+// problem : https://www.spoj.com/problems/HOLI/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
